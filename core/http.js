98142b2c8b68761846caf02ce5e74402a187479c
Shaban-Eissa <shabaneissa767@gmail.com>
2025-01-14 15:50:48 +0200

    💥 NEW: add HttpClient class with GET and POST methods

---
+++ core/http.js
@@ -0,0 +1,15 @@
+export class HttpClient {
+  async get(url) {
+    const response = await fetch(url);
+    return response.json();
+  }
+
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    return response.json();
+  }
+}
